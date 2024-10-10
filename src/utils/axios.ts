52143716b0a6fd2b1600357e4e80ada378696f6e
SSSBoOm <boom1909chanapat@gmail.com>
2024-10-11 00:11:28 +0700

    feat: Setup axios interceptors

---
+++ src/utils/axios.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import { headers } from 'next/headers'
+
+export const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8080',
+  withCredentials: true,
+  timeout: 1000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+axiosInstance.interceptors.request.use((config) => {
+  config.headers['Cookies'] = headers().get('cookie')
+  return config
+})
