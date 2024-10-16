5225d400bf457df84a7b816c14136427063cf1d8
Thanaphol(WD MBP) <thanaphol.iqtha@gmail.com>
2024-10-17 05:03:38 +0700

    Refactor tailwind.config.ts to add custom size and spacing options

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss';
 
+const size = Object.fromEntries(Array.from({ length: 100 }, (_, i) => [`${i + 96}`, `${(i + 96) / 4}rem`]));
 const config: Config = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
@@ -7,6 +8,18 @@ const config: Config = {
       colors: {
         background: 'F5F6FB',
         foreground: 'var(--foreground)',
+      },
+      size: size,
+      spacing: size,
+      borderRadius: {
+        none: '0',
+        sm: '0.125rem',
+        md: '0.375rem',
+        lg: '0.5rem',
+        xl: '0.75rem',
+        '2xl': '1rem',
+        '3xl': '1.5rem',
+        full: '9999px',
       },
     },
   },
